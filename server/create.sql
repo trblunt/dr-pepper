b4867cf1264311142016a3660f498b12d0867dc6
Henry O'Scannlain-Miller <henryom@me.com>
2021-11-03 17:58:03 -0700

    added tables and some sample data

---
+++ server/create.sql
@@ -0,0 +1,87 @@
+DROP SCHEMA Public CASCADE;
+CREATE SCHEMA Public;
+
+CREATE SEQUENCE user_id_seq;
+
+CREATE TABLE SUser (
+    user_id BIGINT NOT NULL DEFAULT NEXTVAL('user_id_seq'),
+    name VARCHAR(100),
+    email VARCHAR(50),
+    address VARCHAR(1000),
+    dob DATE,
+    -- key
+    PRIMARY KEY (user_id)
+);
+
+
+CREATE TABLE Patient (
+    user_id BIGINT,
+    insuranceProvider VARCHAR(50),
+    insuranceID INT,
+    -- key
+    FOREIGN KEY (user_id) REFERENCES SUser(user_id),
+    PRIMARY KEY (user_id)
+);
+
+
+CREATE TABLE Staff (
+    user_id BIGINT,
+    username VARCHAR(50),
+    password VARCHAR(50),
+    isDoctor BOOLEAN, -- this is weird, but the only difference is what
+    -- they can do, docotrs and nurses share all other attributes
+    -- key
+    FOREIGN KEY (user_id) REFERENCES SUser(user_id),
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE Pharmacy (
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(1000),
+    -- key
+    PRIMARY KEY (name)
+);
+
+-- do we need a clinic??
+
+CREATE SEQUENCE visit_id_seq;
+
+CREATE TABLE Visit (
+    visit_id BIGINT NOT NULL DEFAULT NEXTVAL('visit_id_seq'),
+    patient_id SERIAL NOT NULL,
+    date DATE,
+    -- vitals
+    height INT,
+    weight INT,
+    temp FLOAT,
+    bpSystolic INT,
+    bpDiastolic INT,
+    testName VARCHAR(50),
+    testResult VARCHAR(50),
+    -- key
+    FOREIGN KEY (patient_id) REFERENCES Patient(user_id),
+    PRIMARY KEY (visit_id)
+);
+
+CREATE SEQUENCE perscription_id_seq;
+
+CREATE TABLE Perscription (
+    perscription_id BIGINT NOT NULL DEFAULT NEXTVAL('perscription_id_seq'),
+    user_id SERIAL NOT NULL,
+    medicationName VARCHAR(50),
+    quantityPerDay INT,
+    -- key
+    FOREIGN KEY (user_id) REFERENCES Patient(user_id),
+    PRIMARY KEY (perscription_id)
+);
+
+CREATE SEQUENCE allergy_id_seq;
+
+CREATE TABLE Allergy (
+    allergy_id BIGINT NOT NULL DEFAULT NEXTVAL('allergy_id_seq'),
+    user_id SERIAL NOT NULL,
+    name VARCHAR(50),
+    -- key
+    FOREIGN KEY (user_id) REFERENCES Patient(user_id),
+    PRIMARY KEY (allergy_id)
+);
---
+++ server/sample_data.sql
@@ -0,0 +1,39 @@
+
+INSERT INTO SUser VALUES
+-- patients
+(1, 'Henry OM', 'henry@email.com', 'Tempe, AZ', '10/26/2000'),
+(2, 'Winston Smith', 'wsmith@mot.o', 'Oceania, UK', '1/1/1984'),
+-- nurses
+(3, 'Nurse Ratched', 'cuckoo@flewover.com', 'Oregon', '2/1/1962'),
+-- doctors
+(4, 'Dr. Pepper', 'pepper@health.com', 'Tempe, AZ', '7/3/1990'),
+(5, 'Dr. Coke', 'coke@health.com', 'Tempe, AZ', '1/1/1984');
+
+INSERT INTO Patient VALUES
+(1, 'Blue Cross', 4932),
+(2, 'ministry of health', 2139);
+
+
+INSERT INTO Staff VALUES
+-- nurses
+(3, 'nratched', 'evil', FALSE),
+-- drs
+(4, 'pepper', 'pass123', TRUE),
+(5, 'coke', 'secret_recipe', TRUE);
+
+
+INSERT INTO Pharmacy VALUES
+('Dr. Peppers Clinic', 'Tempe, AZ');
+
+
+INSERT INTO Visit VALUES  
+(1, 1, '5/23/2016', 68, 155, 98.3, 100, 80, 'is cool', 'yeah'),
+(2, 1, '9/3/2018', 69, 170, 97.9, 132, 90, 'is still cool', 'yep'),
+(3, 2, '1/1/1984', 52, 120, 100.3, 140, 120, 'is loyal', 'no');
+
+INSERT INTO Perscription VALUES
+(1, 2, 'red pill', 1);
+
+
+INSERT INTO Allergy VALUES
+(1, 2, 'Gluten');
