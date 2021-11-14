51393b944b7a16ccca8920dd37078c19165f1385
henryom <henryom@me.com>
2021-11-13 16:58:08 -0800

    created server.java, added postgress to dependancies. next step is to get values from inputs

--- src/main/java/com/trblunt/DrPepper/PatientSignInController.java
+++ src/main/java/com/trblunt/DrPepper/PatientSignInController.java
@@ -1,15 +1,29 @@
 package com.trblunt.DrPepper;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class PatientSignInController {
 
+
 	@FXML
-	protected void handleSignInAction(ActionEvent event) throws IOException {
-		App.setRoot("Account");
+	protected void handleSignInAction(ActionEvent event) throws IOException, ParseException {
+		Server server = Server.getServer();
+		
+		if (server.validPatientLogin("Henry OM", "2000-10-26")) {
+			App.setRoot("Account");
+		} else {
+			App.setRoot("FrontPage");
+		}
+		
 	}
 
 	@FXML protected void handleBackAction(ActionEvent event) throws IOException {
---
+++ src/main/java/com/trblunt/DrPepper/Server.java
@@ -0,0 +1,58 @@
+package com.trblunt.DrPepper;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import com.trblunt.DrPepper.types.Patient;
+
+
+public class Server {
+
+    private static Server server = new Server();
+    private Connection c = null;
+
+    private Server (){
+        // init the server
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.c = DriverManager.getConnection("jdbc:postgresql://198.58.101.42:5432/pepper", "postgres", "drpepper");
+            System.out.println("Connected!");
+        } catch (Exception e) {
+            System.out.println("Failed to connect!");
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    public static Server getServer(){
+        return server;
+    }
+
+    boolean validPatientLogin(String name, String dob){
+        try {
+            // the cast is nessesary because the Java Date format was not working and Java is enforcing type casting
+            String sql = "SELECT * FROM SUser U, Patient P WHERE U.user_id = P.user_id AND name = ? AND dob = CAST(? AS DATE);";
+            PreparedStatement sm = c.prepareStatement(sql);
+            sm.setString(1, name);
+            sm.setString(2, dob);
+            System.out.println(sm.toString());
+            ResultSet rs = sm.executeQuery();
+            System.out.println(rs);
+            if (rs.next() == true) {
+                return true;
+            }
+            sm.close();
+
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println(e);
+            // System.exit(0);
+        }
+        return false;
+    }
+
+}
