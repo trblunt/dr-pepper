c9161ea909f34089c2d5b74a3a128004a9a53696
trblunt <tommybluntjr@gmail.com>
2021-11-15 16:32:14 -0700

    Add dynamic patient picker

--- src/main/java/com/trblunt/DrPepper/DoctorPickPatientController.java
+++ src/main/java/com/trblunt/DrPepper/DoctorPickPatientController.java
@@ -18,9 +18,9 @@ public class DoctorPickPatientController {
 	@FXML
 	public void initialize() {
 		ObservableList<TitledPane> panes = accordion.getPanes();
-		Patient testPatient = new Patient("John", "Doe", "01/01/2000");
-		for (int i=0; i<4; i++) {
-			PatientPane pane = new PatientPane(testPatient);
+		Patient[] patients = getPatients();
+		for (int i=0; i<patients.length; i++) {
+			PatientPane pane = new PatientPane(patients[i]);
 			pane.isActive.addListener((isActive, oldVal, newVal) -> {
 				if (!newVal) {
 					panes.remove(pane);
@@ -32,6 +32,15 @@ public class DoctorPickPatientController {
 
 	@FXML protected void handleLogoutAction(ActionEvent event) throws IOException {
 		App.setRoot("FrontPage");
+	}
+	
+	private Patient[] getPatients() {
+		//TODO: Get from database
+		return new Patient[] {
+			new Patient("John", "Doe", "01/01/2000"),
+			new Patient("Jane", "Smith", "02/10/2014"),
+			new Patient("John", "Doe", "11/30/1987")
+		};
 	}
 
 	/* @FXML
