70dc26e2b5e5c2c82f284da58856788f418b2a95
henryom <henryom@me.com>
2021-11-17 19:32:52 -0800

    made better fake data

--- server/sample_data.sql
+++ server/sample_data.sql
@@ -1,10 +1,10 @@
 
 INSERT INTO SUser VALUES
 -- patients
-(1, 'Henry OM', 'henry@email.com', 'Tempe, AZ', '10/26/2000'),
+(1, 'Little Moose', 'moose@email.com', 'Tempe, AZ', '1/12/2015'),
 (2, 'Winston Smith', 'wsmith@mot.o', 'Oceania, UK', '1/1/1984'),
 -- nurses
-(3, 'Nurse Ratched', 'cuckoo@flewover.com', 'Oregon', '2/1/1962'),
+(3, 'Nurse Nancy', 'cuckoo@flewover.com', 'Oregon', '2/1/1962'),
 -- doctors
 (4, 'Dr. Pepper', 'pepper@health.com', 'Tempe, AZ', '7/3/1990'),
 (5, 'Dr. Coke', 'coke@health.com', 'Tempe, AZ', '1/1/1984');
@@ -17,7 +17,7 @@ INSERT INTO Patient VALUES
 
 INSERT INTO Staff VALUES
 -- nurses
-(3, 'nratched', 'evil', FALSE),
+(3, 'nancy', '1234', FALSE),
 -- drs
 (4, 'pepper', 'pass123', TRUE),
 (5, 'coke', 'secret_recipe', TRUE);
@@ -28,24 +28,26 @@ INSERT INTO Pharmacy VALUES
 
 
 INSERT INTO Visit VALUES  
-(1, 1, '5/23/2016', 68, 155, 98.3, 100, 80, 'is cool', 'yeah', 'FALSE', 4, 'inflamed sense of self worth'),
-(2, 1, '9/3/2018', 69, 170, 97.9, 132, 90, 'is still cool', 'yep', 'FALSE', 4, 'stubbed toe'),
-(3, 2, '1/1/1984', 52, 120, 100.3, 140, 120, 'is loyal', 'no', 'FALSE', 4, 'got rubbed the wrong way');
+(1, 1, '5/23/2016', 68, 155, 98.3, 100, 80, 'is growing well', 'yep!', 'TRUE', 4, 'first check up'),
+(2, 1, '9/3/2018', 69, 170, 97.9, 132, 90, 'Toe X-Ray', 'no fractures', 'TRUE', 4, 'stubbed toe'),
+(3, 2, '1/1/1984', 52, 120, 100.3, 140, 120, 'Pushup Test', 'failed', 'TRUE', 4, 'suspected of party disloyalty');
 SELECT setval('visit_id_seq', 3, true);
 
 
 INSERT INTO Perscription VALUES
 (1, 2, 'red pill', 1),
-(2, 2, 'blue pill', 1);
-SELECT setval('perscription_id_seq', 2, true);
+(2, 2, 'blue pill', 1),
+(3, 1, 'vitamins', 1);
+SELECT setval('perscription_id_seq', 3, true);
 
 
 INSERT INTO Allergy VALUES
-(1, 2, 'Gluten');
-SELECT setval('allergy_id_seq', 1, true);
+(1, 2, 'Gluten'),
+(2, 1, 'Blackberries');
+SELECT setval('allergy_id_seq', 2, true);
 
 
 INSERT INTO Immunization VALUES
 (1, 2, 'covid-19'),
-(2, 2, 'herpies');
+(2, 1, 'HPV');
 SELECT setval('immunization_id_seq', 2, true);
